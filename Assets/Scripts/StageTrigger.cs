8a615d0f015d9a98609742b27ad7e98c6c8d6fb0
JoseDFerreira <joseduranferreira@gmail.com>
2021-02-04 14:01:34 +0100

    feat: add stage ending and triggers

---
+++ Assets/Scripts/StageTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class StageTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static Action<float> onStageTrigger;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Stage");
+        if (other.gameObject.CompareTag("Player"))
+            onStageTrigger?.Invoke(transform.position.y);
+    }
+}
---
+++ Assets/Scripts/StagesEnding.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StagesEnding : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _differentEndings;
+
+    private void OnEnable() {
+        int rnd = Random.Range(0, _differentEndings.Count);
+        _differentEndings[rnd].SetActive(true);
+    }
+    private void OnDisable() {
+        foreach (var endings in _differentEndings){ }
+    }
+}
