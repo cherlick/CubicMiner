6384b4ca1dd8d44bf63023844de889597857bdc6
JoseDFerreira <joseduranferreira@gmail.com>
2021-02-04 14:04:51 +0100

    feat: realocate and add new managers

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    private bool _isGameRunning;
+    
+    private void OnEnable() {
+        Enemy.onPlayerKill += GameOver;
+    }
+
+    private void OnDisable() {
+        Enemy.onPlayerKill -= GameOver;
+    }
+
+    private void GameOver()
+    {
+        Debug.Log("GameOver");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
