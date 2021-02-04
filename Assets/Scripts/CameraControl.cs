69f7d76735c3198ac9fc3e8a80bd7fd41adc4205
JoseDFerreira <joseduranferreira@gmail.com>
2021-02-04 14:04:10 +0100

    feat: camera control

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private Camera _cam;
+
+    private void Start() {
+        _cam = Camera.main;
+    }
+
+    private void OnEnable() {
+        StageTrigger.onStageTrigger += MoveCamera;
+    }
+
+    private void MoveCamera(float _yPosition)
+    {
+        transform.position = new Vector3(transform.position.x, _yPosition+3,transform.position.z);
+    }
+}
