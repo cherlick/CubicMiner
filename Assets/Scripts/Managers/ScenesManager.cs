a704e92316e94529baebff06b9cefd1e2d2adfc4
JoseDFerreira <joseduranferreira@gmail.com>
2021-02-24 15:13:32 +0100

    feat: add scenemanager

---
+++ Assets/Scripts/Managers/ScenesManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ScenesManager : MonoBehaviour {
+    public void LoadScene(int index){
+        if(index < SceneManager.sceneCountInBuildSettings)
+            SceneManager.LoadScene(index);
+    }
+
+    public void LoadScene(string sceneName){
+        if (IsValidScene(sceneName))
+            SceneManager.LoadScene(sceneName);
+    }
+
+    private bool IsValidScene(string sceneName){
+        int sceneIndex = SceneUtility.GetBuildIndexByScenePath(sceneName);
+        if (sceneIndex>=0) return true;
+        return false;
+    }
+
+    public void RestartScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+}
